18c9f7366461917f667e16d0dae9dfca836de336
djepi29 <j3anpi3rr3g3org3s@email.com>
2023-04-15 00:07:01 -0400

    sql injection prevention

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,4 +1,3 @@
-// {,, name,count } = process.argv;
 // //connect , no options
 // const { Pool } = require('pg');
 
@@ -40,13 +39,31 @@ const pool = new Pool({
 
 // 3rd query with process.argv
 
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+// pool.query(`
+// SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+// FROM students
+// JOIN cohorts ON cohorts.id = cohort_id
+// WHERE cohorts.name LIKE '%${process.argv[2]}%'
+// LIMIT ${process.argv[3] || 5};
+// `)
+
+// preventing sql injections with process.argv
+
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
+  const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
+
 
 // 1st 
 // .then(res => {
@@ -66,10 +83,30 @@ LIMIT ${process.argv[3] || 5};
   })
 })
 .catch(err => console.error('query error', err.stack));
+////////////////////////
+// const { Pool } = require('pg');
+// const [, , cohortName, limit = 5] = process.argv;
 
+// const pool = new Pool({
+//   user: 'djepi',
+//   host: 'localhost',
+//   database: 'bootcampx'
+// });
 
+// const queryString = `
+//   SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+//   FROM students
+//   JOIN cohorts ON cohorts.id = cohort_id
+//   WHERE cohorts.name LIKE $1
+//   LIMIT $2;
+// `;
 
+// const values = [`%${cohortName}%`, limit];
 
-
-
-
+// pool.query(queryString, values)
+//   .then(res => {
+//     res.rows.forEach(user => {
+//       console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+//     });
+//   })
+//   .catch(err => console.error('query error', err.stack)); 
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -6,16 +6,25 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohorts
 FROM teachers 
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name  = '${process.argv[2] || 'JUL02'}'
+WHERE cohorts.name  = $1
 ORDER BY teachers.name
-LIMIT 8;
-`)
+LIMIT $2;
+`;
+
+
+
+const [, , cohortName, limit = 5] = process.argv;
+const values = [`${cohortName}`, limit];
+
+pool.query(queryString, values)
+
+
 // .then(res => {
 //   console.log(res.rows);
 // })
@@ -26,3 +35,4 @@ LIMIT 8;
   })
 })
 .catch(err => console.error('query error', err.stack));
+/////////////////////
